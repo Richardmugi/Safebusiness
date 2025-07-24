af9e2640499eeb72d2c8fd7d46e17c73bd9e3f68
Richardmugi <rmugisha927@gmail.com>
2025-07-24 13:20:34 +0300

    first commit

--- lib/screens/FaceRec/face_registration.dart
+++ lib/screens/FaceRec/face_registration.dart
@@ -123,6 +123,7 @@ class _FaceRegisterPageState extends State<FaceRegisterPage> {
 
       final bytes = await File(file.path).readAsBytes();
       img.Image image = img.decodeImage(bytes)!;
+      image = img.bakeOrientation(image); // ✅ Fix rotation on iOS
 
       final x = face.boundingBox.left.toInt().clamp(0, image.width - 1);
       final y = face.boundingBox.top.toInt().clamp(0, image.height - 1);
