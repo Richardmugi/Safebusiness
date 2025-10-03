72ddf3679456ceb5823c1f22747b24f6750ec1de
Richardmugi <rmugisha927@gmail.com>
2025-10-03 21:21:17 +0300

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 24
+        versionCode = 25
         versionName = "1.0.2"
     }
 
